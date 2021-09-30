f3f6a8dcb50c3e3635f47003c624bf9ef55ad281
DangerousDean <44074418+DangerousDean@users.noreply.github.com>
2021-09-29 23:07:32 -0500

    added proper unit/integration tests and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 Book.create!([
-  {title: "deans book lol", author: "deaner duh", price: 127.27, published_date: "2021-09-29"}
+  {title: "deans book lol", author: "deaner duh", price: 127.27, published_date: "2021-09-29"},
+  {title: "deanbook", author: "me", price: 1.0, published_date: "2021-09-15"},
+  {title: "Dean: 3rd ed.", author: "Deaner", price: 2.0, published_date: "2021-09-16"}
 ])
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,11 +2,53 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid title' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'some guy'
+    fill_in 'Price', with: "12.12"
+    fill_in 'Published date', with: "2021-09-29"
     click_on 'Create Book'
-    visit books_path
+    #visit books_path
     expect(page).to have_content('harry potter')
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid author' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'some guy'
+    fill_in 'Price', with: "12.12"
+    fill_in 'Published date', with: "2021-09-29"
+    click_on 'Create Book'
+    #visit books_path
+    expect(page).to have_content('some guy')
+  end
+end
+
+  RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'some guy'
+    fill_in 'Price', with: "12.12"
+    fill_in 'Published date', with: "2021-09-29"
+    click_on 'Create Book'
+    #visit books_path
+    expect(page).to have_content("12.12")
+  end
+end
+
+  RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid published date' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'some guy'
+    fill_in 'Price', with: "12.12"
+    fill_in 'Published date', with: "2021-09-29"
+    click_on 'Create Book'
+    #visit books_path
+    expect(page).to have_content("2021-09-29")
   end
 end
